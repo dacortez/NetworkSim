a96afeac6f26f5f52df945d6bc8b29c51dafb148
Daniel Cortez <dacortez79@gmail.com>
2016-05-24 22:50:05 -0300

    Adiciona módulo estatístico para geraão de dados

--- __main__.py
+++ __main__.py
@@ -1,19 +1,27 @@
 import sys
 from netsim.netline_parser import NetLineParser
+from netsim.stat import BlockTimeSim
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print 'Uso: python NetworkSim <netline.txt>'
+    if len(sys.argv) < 3:
+        print 'Uso: python NetworkSim <netline.txt> <data_folder>'
         exit(0)
 
     file_name = sys.argv[1]
     parser = NetLineParser(file_name)
 
     print 'Processando arquivo %s...' % file_name
-    parser.parse()
+    #parser.parse()
 
     print 'Airports = %d' % len(parser.network.airports)
     print 'Fleets   = %d' % len(parser.network.fleets)
     print 'Legs     = %d' % len(parser.network.legs)
     print 'Routes   = %d' % len(parser.network.routes)
-    print parser.network
+
+    data_folder = sys.argv[2]
+    btsim = BlockTimeSim(data_folder)
+
+    print 'Processando dados em %s...' % data_folder
+    btsim.init()
+
+    print btsim.get_block('CGH', 'SSA')
---
+++ netsim/stat.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+from scipy import stats
+
+
+class BlockTimeSim:
+
+    def __init__(self, data_folder):
+        self.data_folder = data_folder
+        self.data = {}
+        self.kdes = {}
+
+    def init(self):
+        for fname in os.listdir(self.data_folder):
+            path = '%s/%s' % (self.data_folder, fname)
+            x = np.fromfile(path, dtype=float, sep='\n')
+            pair = fname.split('_')
+            key = '%s%s' % (pair[0], pair[1])
+            self.data[key] = x
+            if len(x) > 1:
+                self.kdes[key] = stats.gaussian_kde(x)
+
+    def get_block(self, fr, to):
+        key = '%s%s' % (fr, to)
+        if key in self.kdes:
+            return self.kdes[key].resample(1)[0][0]
+        else:
+            return -1
